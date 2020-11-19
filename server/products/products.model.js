91d19c2ca18bfc454a5e6fbade36d2e2c44fd427
Ryan Tillaman <37836505+yellow-cyber@users.noreply.github.com>
2020-11-19 18:25:54 +0800

    change number to Number

--- server/products/products.model.js
+++ server/products/products.model.js
@@ -1,30 +1,28 @@
 //AMO INI IT MODEL HIN USER HA DATABASE
 
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 
 //ITO KAILANGAN NIYO SA PAG REGISTER EXCPET SA HASH AND CREATED DATE
 const schema = new Schema({
-    productId: { type: String, unique: true, required: true },
-    productName: { type: String, required: true },
-    ownerId: { type: String, required: true },
-    quantity: { type: String, required: true },
-    sold: { type: String, required: true },
-    description: { type: String, required: true },
-	quantity: { type: number, required: true },
-	sold: { type: number, required: true },
-    createdDate: { type: Date, default: Date.now }
+  productId: { type: String, unique: true, required: true },
+  productName: { type: String, required: true },
+  ownerId: { type: String, required: true },
+  quantity: { type: String, required: true },
+  sold: { type: String, required: true },
+  description: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  sold: { type: Number, required: true },
+  createdDate: { type: Date, default: Date.now },
 });
 
-schema.set('toJSON', {
-    virtuals: true,
-    versionKey: false,
-    transform: function (doc, ret) {
-        delete ret._id;
-        delete ret.hash;
-    }
+schema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+    delete ret.hash;
+  },
 });
 
-module.exports = mongoose.model('Product', schema); 
+module.exports = mongoose.model("Product", schema);
